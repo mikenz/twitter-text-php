b5e5ef769cac29b945b38ecf09c799842948ad28
Nick Pope <nick@nickpope.me.uk>
2010-07-14 19:29:47 +0100

    Fix typo in method name

--- src/Twitter/Extractor.php
+++ src/Twitter/Extractor.php
@@ -5,7 +5,7 @@ class Twitter_Extractor {
   public function extractAll($tweet) {
     return array(
       'hashtags' => $this->extractHashtags($tweet),
-      'urls'     => $this->extraclURLS($tweet),
+      'urls'     => $this->extractURLs($tweet),
       'mentions' => $this->extractMentionedScreennames($tweet),
       'replyto'  => $this->extractReplyScreenname($tweet)
     );
@@ -16,7 +16,7 @@ class Twitter_Extractor {
     return $matches[3];
   }
 
-  public function extraclURLS($tweet) {
+  public function extractURLs($tweet) {
     $URL_VALID_PRECEEDING_CHARS = "(?:[^/\"':!=]|^|\\:)";
     $URL_VALID_DOMAIN = "(?:[\\.-]|[^\\p{P}\\s])+\\.[a-z]{2,}(?::[0-9]+)?";
     $URL_VALID_URL_PATH_CHARS = "[a-z0-9!\\*'\\(\\);:&=\\+\\$/%#\\[\\]\\-_\\.,~@]";
